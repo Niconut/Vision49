2107cb7916184807fa931a1b4b9b3d0a48ff074d
7700BVRC <joshuaschan@icloud.com>
2026-04-13 18:11:54 -0700

    added flywheel class

--- FlywheelOdometrySpeed.java
+++ FlywheelOdometrySpeed.java
@@ -3,4 +3,5 @@ import .com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import .com.qualcomm.robotcore.hardware.DcMotorEx;
 import .com.qualcomm.robotcore.hardware.DcMotor;
 public class FlywheelOdometrySpeed {
+
 }
